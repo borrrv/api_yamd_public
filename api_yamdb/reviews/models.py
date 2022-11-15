709ed832872890a43f99848a27493d3fc5b0486a
Алексей Горячев <A1.Goryachev@yandex.ru>
2022-11-15 12:18:32 +0300

    Added Comment and Review models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,65 @@
+"""Create your models here."""
+
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    title = models.IntegerField(verbose_name='Отзыв')
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Отзыв'
+    )
+    author = models.CharField(verbose_name='Автор')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор'
+    )
+    text = models.TextField(verbose_name='Текст Отзыва')
+    rating = models.IntegerField(
+        verbose_name='Рейтинг',
+        validators=[
+            MinValueValidator(1, 'Рейтинг выставляется по 10 бальной шкале.'),
+            MaxValueValidator(10, 'Рейтинг выставляется по 10 бальной шкале.')
+        ])
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации')
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        constraints = [
+            models.UniqueConstraint(fields=['author', 'title'],
+                                    name='unique_author_title')
+        ]
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='комментарии'
+    )
+    author = models.CharField(verbose_name='Автор')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор'
+    )
+    text = models.TextField(verbose_name='Текст комментария')
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания')
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
