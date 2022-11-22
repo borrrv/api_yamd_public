a485b9d4888b8ce2ff3b1754bace3149757efcf6
Алексей Горячев <A1.Goryachev@yandex.ru>
2022-11-22 13:28:22 +0300

    removed validators.py

--- api_yamdb/reviews/validators.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-
-def username_validate(value):
-    """Валидатор недопустимого usernmae - me."""
-    if value == 'me':
-        raise serializers.ValidationError(f"Недопустимо имя '{value}'")
-    return value
